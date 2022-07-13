8d0314f5b430260d5b5e18d3950c4177b74f80b9
Roman121082 <106985412+Roman121082@users.noreply.github.com>
2022-07-13 22:47:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+   $('.popup__link').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+   });
+   $('.gallery__slider').slick({
+      prevArrow: '<button type="button" class="slick__btn slick-prev"><img src="images/gallery/arrow-left.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick__btn slick-next"><img src="images/gallery/arrow-right.svg" alt=""></button>'
+   });
+
+   $('.gallery__item-inner').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+         enabled: true,
+         navigateByImgClick: true,
+         preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+
+   });
+
+   $('.menu__btn').on('click', function () {
+      $('.menu__list').toggleClass('menu__list--active');
+   });
+
+
+
+});
